83fb0e3f46f6ac01c0bf6eebaf7f7c3c65fdc6cd
Maiss Dev <maissen.developer500@gmail.com>
2023-05-10 19:30:28 +0200

    Work more on Aside menu : _when you click on a link it gets active

--- js/aside-menu.js
+++ js/aside-menu.js
@@ -1,2 +1,15 @@
 const aside_menu = document.querySelector('#aside-menu');
 const aside_menu_behind = document.querySelector('#aside-menu__behind');
+
+
+function loadContent(page, link){
+
+    // Remove the 'active' class from all links in the side navigation menu
+    var links = document.querySelectorAll('.aside-menu__item');
+    for (var i = 0; i < links.length; i++) {
+        links[i].classList.remove('active-aside-menu-item');
+    }
+    
+    // Add the 'active' class to the clicked link element
+    link.classList.add('active-aside-menu-item');
+}
